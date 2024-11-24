dd2f993904be3ddf12afc00d4327947934a8db4f
mohammadfaramarzi1 <faramarzi.dev@gamil.com>
2024-11-24 20:23:59 +0330

    feat: add routing and basic components

---
+++ src/routes.js
@@ -0,0 +1,13 @@
+import Home from "./pages/Home/Home";
+import NewUser from "./pages/NewUser/NewUser";
+import Products from "./pages/Products/Products";
+import UsersList from "./pages/UsersList/UsersList";
+
+let routes = [
+  { path: "/", element: <Home /> },
+  { path: "/users", element: <UsersList /> },
+  { path: "newUser", element: <NewUser /> },
+  { path: "products", element: <Products /> },
+];
+
+export default routes;
