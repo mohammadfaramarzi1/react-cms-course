d35aa242b9244e9098374a5e343f2eeb2b70cd39
mohammadfaramarzi1 <faramarzi.dev@gamil.com>
2024-11-25 11:53:47 +0330

    style: complete styles of SideBar.css and TopBar.css

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -26,16 +26,16 @@ function SideBar() {
           <h3 className="sidebar__title">Dashboard</h3>
           <ul className="sidebar__list">
             <Link to="/" className="link">
-              <li className="sidebar__link-item active">
+              <li className="sidebar__list-item active">
                 <LineStyleIcon className="sidebar__icon" />
                 Home
               </li>
             </Link>
-            <li className="sidebar__listItem">
+            <li className="sidebar__list-item">
               <TimelineIcon className="sidebar__icon" />
               Analytics
             </li>
-            <li className="sidebar__listItem">
+            <li className="sidebar__list-item">
               <TrendingUpIcon className="sidebar__icon" />
               Sales
             </li>
@@ -47,28 +47,28 @@ function SideBar() {
           <h3 className="sidebar__title">Quick Menu</h3>
           <ul className="sidebar__list">
             <Link to="/users" className="link">
-              <li className="sidebar__listItem">
+              <li className="sidebar__list-item">
                 <PermIdentityIcon className="sidebar__icon" />
                 Users
               </li>
             </Link>
             <Link to="/newUser" className="link">
-              <li className="sidebar__listItem">
+              <li className="sidebar__list-item">
                 <PermIdentityIcon className="sidebar__icon" />
                 New User
               </li>
             </Link>
             <Link to="/products" className="link">
-              <li className="sidebar__listItem">
+              <li className="sidebar__list-item">
                 <StorefrontIcon className="sidebar__icon" />
                 Products
               </li>
             </Link>
-            <li className="sidebar__listItem">
+            <li className="sidebar__list-item">
               <AttachMoneyIcon className="sidebar__icon" />
               Transactions
             </li>
-            <li className="sidebar__listItem">
+            <li className="sidebar__list-item">
               <BarChartIcon className="sidebar__icon" />
               Reports
             </li>
@@ -79,15 +79,15 @@ function SideBar() {
         <div className="sidebar__menu">
           <h3 className="sidebar__title">Notifications</h3>
           <ul className="sidebar__list">
-            <li className="sidebar__listItem">
+            <li className="sidebar__list-item">
               <MailOutlineIcon className="sidebar__icon" />
               Mail
             </li>
-            <li className="sidebar__listItem">
+            <li className="sidebar__list-item">
               <DynamicFeedIcon className="sidebar__icon" />
               Feedback
             </li>
-            <li className="sidebar__listItem">
+            <li className="sidebar__list-item">
               <ChatBubbleOutlineIcon className="sidebar__icon" />
               Messages
             </li>
@@ -98,15 +98,15 @@ function SideBar() {
         <div className="sidebar__menu">
           <h3 className="sidebar__title">Staff</h3>
           <ul className="sidebar__list">
-            <li className="sidebar__listItem">
+            <li className="sidebar__list-item">
               <WorkOutlineIcon className="sidebar__icon" />
               Manage
             </li>
-            <li className="sidebar__listItem">
+            <li className="sidebar__list-item">
               <TimelineIcon className="sidebar__icon" />
               Analytics
             </li>
-            <li className="sidebar__listItem">
+            <li className="sidebar__list-item">
               <ReportIcon className="sidebar__icon" />
               Reports
             </li>
