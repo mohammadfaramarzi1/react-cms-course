24de95ae782bd8cacf0ebcc2a8e61303c5544132
mohammadfaramarzi1 <faramarzi.dev@gamil.com>
2024-11-25 10:56:55 +0330

    feat: SideBar.jsx dashboard section

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import "./SideBar.css"
+import LineStyleIcon from '@mui/icons-material/LineStyle';
+import TimelineIcon from '@mui/icons-material/Timeline';
+import TrendingUpIcon from '@mui/icons-material/TrendingUp';
 
 function SideBar() {
   return (
-    <div>SideBar</div>
+    <div className='sidebar'>
+        <div className="sidebar-wrapper">
+            <div className="sidebar__menu">
+                <h3 className='sidebar__title'>Dashboard</h3>
+                <ul className='sidebar__list'>
+                    <li className='sidebar__list-item'>
+                        <LineStyleIcon className="sidebar__icon" />
+                        Home
+                    </li>
+                    <li className='sidebar__list-item'>
+                        <TimelineIcon className="sidebar__icon" />
+                        Analytics
+                    </li>
+                    <li className='sidebar__list-item'>
+                        <TrendingUpIcon className="sidebar__icon" />
+                        Sales
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
   )
 }
 
