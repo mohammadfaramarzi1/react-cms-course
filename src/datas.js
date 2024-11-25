1ecab7607a6bfc851b9a41a821db1b46ec47144d
mohammadfaramarzi1 <faramarzi.dev@gamil.com>
2024-11-25 19:38:47 +0330

    feat: xAxisData feature

---
+++ src/datas.js
@@ -0,0 +1,50 @@
+let xAxisData = [
+  {
+    name: "Jan",
+    sale: 112_000,
+  },
+  {
+    name: "Feb",
+    sale: 99_000,
+  },
+  {
+    name: "Mar",
+    sale: 12_090,
+  },
+  {
+    name: "Apr",
+    sale: 99_000,
+  },
+  {
+    name: "May",
+    sale: 112_000,
+  },
+  {
+    name: "Jun",
+    sale: 54_000,
+  },
+  {
+    name: "Jul",
+    sale: 85_000,
+  },
+  {
+    name: "Aug",
+    sale: 75_000,
+  },
+  {
+    name: "Sep",
+    sale: 34_000,
+  },
+  {
+    name: "Oct",
+    sale: 19_000,
+  },
+  {
+    name: "Nov",
+    sale: 0,
+  },
+  {
+    name: "Dec",
+    sale: 73_000,
+  },
+];
