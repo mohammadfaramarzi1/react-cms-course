2e575d1c40409e066eb0bfedd05f423f0375b193
mohammadfaramarzi1 <faramarzi.dev@gamil.com>
2024-11-27 22:11:29 +0330

    feat: WidgetSm.jsx template

--- src/components/WidgetSm/WidgetSm.jsx
+++ src/components/WidgetSm/WidgetSm.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import VisibilityIcon from "@mui/icons-material/Visibility";
 
 function WidgetSm() {
   return (
-    <div>WidgetSm</div>
-  )
+    <div className="widgetsm">
+      <span className="widgetsm__title">New Join Members</span>
+      <ul className="widgetsm__list">
+        <li className="widgetsm__list-item">
+          <img
+            src="images/5886539613504389703.jpg"
+            class="widgetsm__img"
+            alt="user widget"
+          />
+        </li>
+        <div className="widgetsm__user">
+          <span className="widgetsm__username">Mohammad Faramarzi</span>
+          <span className="widgetsm__user-title">Front-End Developer</span>
+        </div>
+        <button className="widgetsm__button">
+          <VisibilityIcon className="widgetsm__icon" />
+        </button>
+      </ul>
+    </div>
+  );
 }
 
-export default WidgetSm
+export default WidgetSm;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,13 +2,16 @@ import React from "react";
 import Features from "../../components/Features/Features";
 import Chart from "../../components/Chart/Chart";
 import { xAxisData } from "../../datas";
+import WidgetSm from "../../components/WidgetSm/WidgetSm";
 
 function Home() {
   return (
     <diV className="home">
       <Features />
       <Chart grid title="Month Sale" data={xAxisData} dataKey="sale" />
-      <div className="home-widgets"></div>
+      <div className="home-widgets">
+        <WidgetSm />
+      </div>
     </diV>
   );
 }
