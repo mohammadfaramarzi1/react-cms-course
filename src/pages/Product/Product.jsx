5a1d0a36ad941688f060a6fd16edd33916fc0b1d
mohammadfaramarzi1 <faramarzi.dev@gamil.com>
2024-12-01 21:45:53 +0330

    feat: first template Product.jsx file

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import { Link } from "react-router-dom";
+import "./Product.css";
 
 function Product() {
   return (
-    <div>Product</div>
-  )
+    <div className="product">
+      <div className="product__title-container">
+        <h1 className="product__title">Product</h1>
+        <Link to="/newProduct" className="link">
+          <button className="product__add-btn">Create</button>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
-export default Product
+export default Product;
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 import Home from "./pages/Home/Home";
 import NewUser from "./pages/NewUser/NewUser";
+import Product from "./pages/Product/Product";
 import Products from "./pages/Products/Products";
 import UsersList from "./pages/UsersList/UsersList";
 
@@ -8,6 +9,7 @@ let routes = [
   { path: "/users", element: <UsersList /> },
   { path: "newUser", element: <NewUser /> },
   { path: "products", element: <Products /> },
+  { path: "product/:productID", element: <Product /> },
 ];
 
 export default routes;
