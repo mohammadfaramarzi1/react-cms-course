20f54d5a22097beb9ebdf8d313c5c9ddebac1db2
mohammadfaramarzi1 <faramarzi.dev@gamil.com>
2024-12-02 09:36:08 +0330

    feat: third template Product.jsx

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -18,7 +18,34 @@ function Product() {
         <div className="product-top-left">
           <Chart title="Sale In Month" data={productsData} dataKey="sale" />
         </div>
-        <div className="product-top-right"></div>
+        <div className="product-top-right">
+          <div className="product-info-top">
+            <img src="./images/dell.jpg" alt="product" className="product-info__img" />
+            <span className="product-info__name">Dell</span>
+          </div>
+          <div className="product-info-bottom">
+            <div className="product-info__item">
+              <div className="product-info__key">ID:</div>
+              <div className="product-info__value">132</div>
+            </div>
+            <div className="product-info__item">
+              <div className="product-info__key">Name:</div>
+              <div className="product-info__value">Dell</div>
+            </div>
+            <div className="product-info__item">
+              <div className="product-info__key">Sales:</div>
+              <div className="product-info__value">$200000</div>
+            </div>
+            <div className="product-info__item">
+              <div className="product-info__key">Active:</div>
+              <div className="product-info__value">Yes</div>
+            </div>
+            <div className="product-info__item">
+              <div className="product-info__key">In Store:</div>
+              <div className="product-info__value">No</div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
